3534404a22bb50d3f210c3bfdacb0b330c473d13
BlueHatbRit <myalt94@gmail.com>
2016-09-27 16:16:51 +0100

    turn off protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # We're building an API so we don't want this on anything really.
+  # We're also just prototyping so lets keep life simple.
+  #protect_from_forgery with: :exception
 end
